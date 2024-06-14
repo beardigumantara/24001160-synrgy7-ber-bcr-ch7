be2747b63db0e554cb7c53e4819556ccb3e9f42c
Berdi Gumantara <btara@student.gunadarma.ac.id>
2024-06-14 16:05:30 +0700

    feat: add footer component

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 // import styles from './home.module.css'
 import Header from '../../components/header/header';
+import Footer from '../../components/footer/footer';
 
 const Home: React.FC = () => {
   return (
     <div>
-      <Header/>
+      <Header />
       <h1>Home</h1>
+      <Footer />
     </div>
   )
 }
