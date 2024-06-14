11743fe68f99b38ac36708f87404c34d67976bd0
Berdi Gumantara <btara@student.gunadarma.ac.id>
2024-06-14 15:16:45 +0700

    feat: add Header Component

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import styles from './home.module.css'
+import Header from '../../components/header/header';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Header/>
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+export default Home;
