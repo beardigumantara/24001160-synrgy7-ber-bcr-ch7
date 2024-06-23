18c9eccc37b7b9dd068cb95da68d4af6fb60aaa3
Berdi Gumantara <btara@student.gunadarma.ac.id>
2024-06-23 20:11:22 +0700

    feat: fix page admin dashboard

--- src/pages/admin/admin.tsx
+++ src/pages/admin/admin.tsx
@@ -1,20 +1,32 @@
 import React from "react";
-import CardAdmin from "../../components/cards/cardAdmin";
+// import CardAdmin from "../../components/cards/cardAdmin";
+import Sidebar from "../../components/sidebar/sidebar";
+import NavbarAdmin from "../../components/navbar/navbarAdmin";
+import styles from "./admin.module.css";
+import TableCars from "../../components/cards/tableCars";
 
 const Admin: React.FC = () => {
-
   const handleLogout = () => {
     localStorage.removeItem("token");
     window.location.href = "/login";
-  }
+  };
 
   return (
-    <div>
-      <h1>Admin Page</h1>
-      <CardAdmin />
-      <button onClick={handleLogout}>Logout</button>
+    <div className="d-flex flex-row" style={{backgroundColor: "#F4F5F7"}}>
+      <Sidebar />
+      <div>
+        <div
+          className="d-flex flex-row justify-content-between align-items-center"
+          style={{ height: "70px", width: "1230px", backgroundColor: "#FFFFFF", boxShadow: "2px 10px 18px #EBEFF5"}}
+        >
+          <NavbarAdmin />
+          <button onClick={handleLogout} id={styles.btnBlue}>Logout</button>
+        </div>
+        {/* <CardAdmin /> */}
+        <TableCars />
+      </div>
     </div>
   );
-}
+};
 
 export default Admin;
